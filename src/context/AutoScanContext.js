f06047e97759258909b5ff398deb032820c751d4
Pelyn9 <peejaymarco@gmail.com>
2025-12-03 12:06:04 +0800

    fetch

--- src/context/AutoScanContext.js
+++ src/context/AutoScanContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useRef, useEffect, useCallback } from "react";
-import { supabase } from "../supabaseClient";
+import { supabase } from "../supabaseClient.js";
 
 export const AutoScanContext = createContext();
 
